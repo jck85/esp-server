040bfc22f84779713a7888bb2b0be072a3da1d11
Justin Kirk <justin.c.kirk@gmail.com>
2025-10-15 22:27:52 -0400

    mvp

--- esp_server/esp_server.ino
+++ esp_server/esp_server.ino
@@ -10,16 +10,36 @@
 #include <AsyncTCP.h>
 #include <WiFi.h>
 
+#include "ArduinoJson-v7.4.2.h"
 #include "../secrets/wifi_creds.h"
+#include "index.h"
+
+const char *param_1 = "gpio";
+const char *param_2 = "state";
 
 static AsyncWebServer server(80);
 static AsyncLoggingMiddleware requestLogger;
 
-void setup() {
+// Process index_html and add template
+String processor(const String &var)
+{
+  Serial.println(var);
+  if (var == "BUTTON_TEMPALTE")
+  {
+    String button_string = "<button>button</button>";
+    return button_string;
+  }
+
+  return String();
+}
+
+void setup()
+{
   Serial.begin(115200);
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
@@ -30,13 +50,40 @@ void setup() {
   requestLogger.setOutput(Serial);
   server.addMiddleware(&requestLogger);
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", "Hello, world!");
-  });
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/html", index_html, processor); });
+
+  // Send a GET request to <ESP_IP>/update?output=<msg_1>&state=<inputMessage2>
+  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String gpio_num;
+              String gpio_state;
+
+              JsonDocument doc;
+              
+              // GET /update?gpio=<gpio_num>&state=<gpio_state>
+              if (request->hasParam("gpio") && request->hasParam("state")) {
+                gpio_num = request->getParam(param_1)->value();
+                gpio_state = request->getParam(param_2)->value();
+              }
+              else {
+                gpio_num = "null";
+                gpio_state = "null";
+              }
+
+                doc["gpio_num"] = gpio_num;
+                doc["gpio_state"] = gpio_state;
+                serializeJsonPretty(doc, Serial);
+
+                String response;
+                serializeJson(doc, response);
+                request->send(200, "application/json", response); });
 
   server.begin();
 }
 
-void loop() {
+void loop()
+{
   delay(100);
 }
--- esp_server/index.h
+++ esp_server/index.h
@@ -1,26 +1,133 @@
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
+<!DOCTYPE HTML>
+<html>
+
 <head>
-    <title>esp_server</title>
+    <title>ESP Web Server</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="data:,">
-    <style> 
+    <style>
         html {
             font-family: monospace;
-            font-size: 100%;
         }
 
         body {
             margin: 0;
+            background-color: grey;
         }
+
+        header {
+            display: flex;
+            align-items: center;
+        }
+
+        header > h2 {
+            font-size: 3.0rem;
+        }
+
+        p {
+            font-size: 3.0rem;
+        }
+
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 120px;
+            height: 68px
+        }
+
+        .switch input {
+            display: none
+        }
+
+        .slider {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            border-radius: 6px
+        }
+
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 52px;
+            width: 52px;
+            left: 8px;
+            bottom: 8px;
+            background-color: white;
+            -webkit-transition: .4s;
+            transition: .4s;
+            border-radius: 3px
+        }
+
+        input:checked+.slider {
+            background-color: green;
+        }
+
+        input:checked+.slider:before {
+            -webkit-transform: translateX(52px);
+            -ms-transform: translateX(52px);
+            transform: translateX(52px)
+        }
+
     </style>
 </head>
 
 <body>
-    <h2>ESP Server</h2>
+    <header>
+        <h2>ESP Web Server</h2>
+    </header>
+
+    <div id="button-template">
+        %BUTTON_TEMPALTE%
+    <div>
+
+    <div id="gpio">
+        <label class="switch">
+            GPIO 4
+            <input type="checkbox" onchange="toggleCheckbox(this)" id="4">
+            <span class="slider"></span>
+        </label>
+
+        <label class="switch">
+        GPIO 5
+            <input type="checkbox" onchange="toggleCheckbox(this)" id="5">
+            <span class="slider"></span>
+        </label>
+
+        <label class="switch">
+            GPIO 6
+            <input type="checkbox" onchange="toggleCheckbox(this)" id="6">
+            <span class="slider"></span>
+        </label>"
+    </div>
 
     <script>
-        console.log("esp_server")
+        function toggleCheckbox(element) {
+            console.log("checkbox: ", element.id, element.checked);
+
+            var xhr = new XMLHttpRequest();
+            
+            let state = element.checked ? "HIGH" : "LOW";
+
+            xhr.open("GET", "/update?gpio=" + element.id + "&state=" + state, true);
+
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    const res = xhr.responseText;
+                    const data = JSON.parse(res);
+                    console.log("response: ", res);
+                } else {
+                    console.log("request failed");
+                }
+            }
+
+            xhr.send();
+
+        }
     </script>
 </body>
 
